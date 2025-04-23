dc0a80568a5fa67a92044785e811af25119c374e
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-23 11:20:37 -0300

    header

---
+++ src/components/HeaderCatalog/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+export default function HeaderCatalog(){
+    return(
+        <header className='dsc-header-catalog'>
+            <nav className='dsc-container'>
+                <h1>Carros Top</h1>
+            </nav>
+        </header>
+    );
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,9 @@
+import HeaderCatalog from '../../components/HeaderCatalog';
+import './styles.css'
+export default function Catalog(){
+    return (
+        <>
+        <HeaderCatalog/>
+        </>
+    );
+}
