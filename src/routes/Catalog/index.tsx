c1b665e26649d88ca41723c90efe700fa82ee6ed
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-23 14:16:53 -0300

    SearchBar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,9 +1,16 @@
 import HeaderCatalog from '../../components/HeaderCatalog';
+import SearchBar from '../../components/SearchBar';
 import './styles.css'
 export default function Catalog(){
     return (
         <>
         <HeaderCatalog/>
+        <main>
+            <section id="catalog-section" className="dsc-container">
+            <SearchBar />
+                
+            </section>
+        </main>
         </>
     );
 }
