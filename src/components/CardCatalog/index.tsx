d0aae8cbba8fd3ec1988c85043255824cea24d13
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-25 11:50:37 -0300

    refactoring 2/2

--- src/components/CardCatalog/index.tsx
+++ src/components/CardCatalog/index.tsx
@@ -5,16 +5,10 @@ import './styles.css';
 export default function CardCatalog() {
     return (
         <div className='dsc-card'>
-            <div className='dsc-catalog-card-top'>
                 <img src={carImg} alt="Carro" />
-            </div>
-            <div className='dsc-catalog-card-bottom'>
-                <h2>Audi Supra TT</h2>
+                <h3>Audi Supra TT</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
-            </div>
-            <div className="dsc-catalog-card-btn">
                 <ButtonBuy/>
-            </div>
         </div>
     );
 }
